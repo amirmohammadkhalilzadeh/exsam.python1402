e721d4bf071d6f3c25d99d11b400483370ee8e8a
amirmohammadkhalilzadeh <141909499+amirmohammadkhalilzadeh@users.noreply.github.com>
2023-09-09 21:04:03 +0330

    Add files via upload

---
+++ question30.py
@@ -0,0 +1,9 @@
+def fun(a,b):
+    s=a+b
+    print("sum is a+b = ",s)
+
+    x=a*b
+    print("multiply is a*b = ",x)
+a=int(input("enter a first number:"))
+b=int(input("enter a second number:"))
+fun(a,b)
